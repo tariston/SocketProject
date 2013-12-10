ca224053151eb66211740e5b7928a4bad6ffb3c9
Thomas Ariston <tariston@gmail.com>
2013-12-10 04:37:24 -0800

    initial

---
+++ CSCI156Project/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI156Project
+{
+    class Program
+    {
+        private static Socket _serverSocket = new Socket
+            (AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+        private const int _bufferSize = 1024;
+        private static readonly byte[] _buffer = new byte[_bufferSize];
+        private const int _port = 65001;
+        private static List<Socket> _sockets = new List<Socket>();
+
+        private static void ServerInit()
+        {
+            Console.WriteLine("Server initializing");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, _port));
+            _serverSocket.Listen(10);
+            _serverSocket.BeginAccept(AsyncAcceptCallback, null);
+            Console.WriteLine("Server Listening");
+        }
+
+        private static void AsyncAcceptCallback(IAsyncResult ar)
+        {
+            Socket remoteSocket;
+            try
+            {
+                remoteSocket = _serverSocket.EndAccept(ar);
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+
+            Console.WriteLine("Client connected");
+            _sockets.Add(remoteSocket);
+            remoteSocket.BeginReceive(_buffer, 0, _bufferSize, SocketFlags.None, AsyncRecieveCallback, remoteSocket);
+            _serverSocket.BeginAccept(AsyncAcceptCallback, null);
+        }
+
+        private static void AsyncRecieveCallback(IAsyncResult ar)
+        {
+            Socket clientSocket = (Socket)ar.AsyncState;
+            int dataSize;
+            try
+            {
+                dataSize = clientSocket.EndReceive(ar);
+            }
+            catch (SocketException)
+            {
+                clientSocket.Close();
+                _sockets.Remove(clientSocket);
+                return;
+            }
+            byte[] temp = new byte[dataSize];
+            Array.Copy(_buffer, temp, dataSize);
+            string data = Encoding.ASCII.GetString(temp);
+            //Logic for bidding
+
+            //End Logic
+            clientSocket.BeginReceive(_buffer, 0, _bufferSize, SocketFlags.None, AsyncRecieveCallback, clientSocket);
+        }
+
+        static void Main(string[] args)
+        {
+            ServerInit();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    
+    class Program
+    {
+        private static readonly Socket _socket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int _port = 65001;
+
+        private static void connect()
+        {
+            while (!_socket.Connected)
+            {
+                try
+                {
+                    _socket.Connect(IPAddress.Loopback, _port);
+                }
+                catch(SocketException e)
+                {
+                    Console.WriteLine("Exception" + e.InnerException.Message);
+                }
+            }
+            runLoop();
+        }
+
+        private static void runLoop()
+        {
+            while(true)
+            {
+                sendData();
+                recieveData();
+            }
+        }
+
+        private static void recieveData()
+        {
+            byte[] data = new byte[1024];
+            int length = _socket.Receive(data, SocketFlags.None);
+            byte[] trimmed = new byte[length];
+            Array.Copy(data, trimmed, length);
+            Console.WriteLine(Encoding.ASCII.GetString(trimmed));
+        }
+
+        private static void sendData()
+        {
+            byte[] data = Encoding.ASCII.GetBytes("print time");
+            _socket.Send(data, 0, data.Length, SocketFlags.None);
+        }
+
+        static void Main(string[] args)
+        {
+            connect();
+            Console.ReadLine();
+        }
+    }
+}
